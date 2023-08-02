d6143977dd7566874f8df7318791ec61930ea174
Harry Vu <harryvu@outlook.com>
2023-08-02 07:34:01 -0700

    adds product info

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,6 +43,8 @@ const Home = ({ products }: { products: any }) => {
             return (
               <li className='place-self-center sm:h-auto sm:square sm:self-center md:h-auto md:square' key={product.id}>
                 <Image sizes='(min-width: 200px)' layout='intrinsic' width={400} height={0} src={product.attributes.image.data.attributes.url} alt={product.attributes.code}/>
+                <p className='text-center'>{product.attributes.code}-{product.attributes.name}</p>
+                <p className='text-center'>{product.attributes.barcode}</p>               
               </li>)
           })}
       </ul>
